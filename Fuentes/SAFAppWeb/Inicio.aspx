b95c520aece0cda5682b679b00ee7ca3b5829de9
German Alvarez <kenchic@gmail.com>
2013-04-03 17:41:34 -0500

    Diseño Pagina Inicial

--- Fuentes/SAFAppWeb/Inicio.aspx
+++ Fuentes/SAFAppWeb/Inicio.aspx
@@ -5,16 +5,13 @@
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
+<head id="Head1" runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-    
-        <telerik:RadButton ID="RadButton1" runat="server" Text="RadButton">
-        </telerik:RadButton>
     
     </div>
     </form>
--- Fuentes/SAFAppWeb/Site1.Master.designer.cs
+++ Fuentes/SAFAppWeb/Site1.Master.designer.cs
@@ -10,16 +10,16 @@
 namespace SAFAppWeb {
     
     
-    public partial class Inicio {
+    public partial class Site1 {
         
         /// <summary>
-        /// Head1 control.
+        /// head control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
         /// form1 control.
@@ -29,5 +29,14 @@ namespace SAFAppWeb {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
