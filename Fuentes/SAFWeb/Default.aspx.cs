c6d2fa0de6085b9fe253dc16f766668be1f671c7
German Alvarez <kenchic@gmail.com>
2015-09-05 12:14:47 -0500

    Actualizacion
    Actualizar script base de datos

--- Fuentes/SAFWeb/Default.aspx.cs
+++ Fuentes/SAFWeb/Default.aspx.cs
@@ -15,15 +15,15 @@ namespace SAFWeb
             body.Style["background-image"] = Page.ResolveUrl(url);
             body.Style["background-size"] = "cover";
 
-            var ejecutor = (BaseDatos.Comandos)Session["ejecutorBD"];
-            var fachadaClaseBase = new Seguridad.DALC.clsFachadaClaseBase(ref ejecutor);
-            var usuario = fachadaClaseBase.ConsultarUsuariosAutenticacion(InicioSesion.UserName, InicioSesion.Password);
-            if (usuario == null)
-            {
-                e.Authenticated = false;
-                InicioSesion.FailureText = "Nombre de usuario ó contraseña inválida";
-                return;
-            }
+            //var ejecutor = (BaseDatos.Comandos)Session["ejecutorBD"];
+            //var fachadaClaseBase = new Seguridad.DALC.clsFachadaClaseBase(ref ejecutor);
+            //var usuario = fachadaClaseBase.ConsultarUsuariosAutenticacion(InicioSesion.UserName, InicioSesion.Password);
+            //if (usuario == null)
+            //{
+            //    e.Authenticated = false;
+            //    InicioSesion.FailureText = "Nombre de usuario ó contraseña inválida";
+            //    return;
+            //}
         }
 
     }
--- Fuentes/SAFWeb/Global.asax.cs
+++ Fuentes/SAFWeb/Global.asax.cs
@@ -1,34 +1,41 @@
 using System;
-    using System.Collections.Generic;
-    using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
 using BaseDatos;
- 
 
-    namespace SAFWeb {
-        public class Global_asax : System.Web.HttpApplication {
-            void Application_Start(object sender, EventArgs e) {
-                DevExpress.Web.ASPxWebControl.CallbackError += new EventHandler(Application_Error);
-                var ejecutor = new BaseDatos.Comandos("Data Source=localhost;Initial Catalog=SAF; User ID=saf; Password=K9saf126", "System.Data.SqlClient", "pDATOS");
-                Session["ejecutorBD"] = ejecutor;
-            }
 
-            void Application_End(object sender, EventArgs e) {
-                // Code that runs on application shutdown
-            }
+namespace SAFWeb
+{
+    public class Global_asax : System.Web.HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            DevExpress.Web.ASPxWebControl.CallbackError += new EventHandler(Application_Error);
+            var ejecutor = new BaseDatos.Comandos("Data Source=localhost;Initial Catalog=SAF; User ID=saf; Password=K9saf126", "System.Data.SqlClient", "pDATOS");
+            //Session["ejecutorBD"] = ejecutor;
+        }
 
-            void Application_Error(object sender, EventArgs e) {
-                // Code that runs when an unhandled error occurs
-            }
+        void Application_End(object sender, EventArgs e)
+        {
+            // Code that runs on application shutdown
+        }
 
-            void Session_Start(object sender, EventArgs e) {
-                // Code that runs when a new session is started
-            }
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+        }
 
-            void Session_End(object sender, EventArgs e) {
-                // Code that runs when a session ends. 
-                // Note: The Session_End event is raised only when the sessionstate mode
-                // is set to InProc in the Web.config file. If session mode is set to StateServer 
-                // or SQLServer, the event is not raised.
-            }
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Code that runs when a new session is started
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            // Code that runs when a session ends. 
+            // Note: The Session_End event is raised only when the sessionstate mode
+            // is set to InProc in the Web.config file. If session mode is set to StateServer 
+            // or SQLServer, the event is not raised.
         }
     }
+}
