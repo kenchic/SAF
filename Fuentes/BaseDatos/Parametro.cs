b01e6206d502df064b1852f2e6ddc54e7cb6f01c
German Alavarez <kenchic@gmail.com>
2015-09-07 23:06:16 -0500

    Actualizar BaseDatos

---
+++ Fuentes/BaseDatos/Parametro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDatos
+{
+    public class ParametroBD
+    {
+        public enum OperadorLogico
+        {
+            igual,
+            diferente,
+            mayor,
+            mayorOIgual,
+            menor,
+            menorOIgual,
+            contiene,
+            entre
+        }
+
+        public string Campo;
+        public object Valor;
+        public OperadorLogico Operador;
+
+        public ParametroBD(string campo, object valor, OperadorLogico operador)
+        {
+            Campo = campo;
+            Valor = valor;
+            Operador = operador;
+        }
+    }
+}
