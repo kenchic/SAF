4d7c420761cfaca01f3b800ee8bd719fdfd5dcb8
German Alavarez <kenchic@gmail.com>
2015-10-14 22:53:54 -0500

    Delete DALC

---
+++ Fuentes/SAFCore/DALC/clsDocumentoAdicionalDALC.cs
@@ -0,0 +1,30 @@
+﻿///////////////////////////////////////////////////////////
+//  clsDocumentoDALC.cs
+//  Implementation of the Class DALC Documento
+//  SAF - Integral Solutions SAS
+//  Created on:      05-abr-2013 12:08:31 p.m.
+//  Original author: German Alvarez
+///////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using BaseDatos;
+
+namespace SAFCore
+{
+    public partial class clsDocumentoDALC
+    {
+        
+
+        public long consultarSiguienteDocumento(long idDocumento)
+        {
+          
+            EjecutorBaseDatos.limpiarParametros();
+            string sentenciaSQL = " Select Numero From bdDocumento Where Id = @IdDocumento";
+            //EjecutorBaseDatos.agregarParametro(filtro.Campo, filtro.Valor);
+            return 0;
+        }
+    }
+}
