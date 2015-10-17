b90468f099a7c873cf6a34088de4b510da83184d
German Alvarez <kenchic@gmail.com>
2015-10-17 12:06:26 -0500

    Copia BD

--- Fuentes/SAFCore/DALC/clsDocumentoAdicionalDALC.cs
+++ Fuentes/SAFCore/DALC/clsDocumentoAdicionalDALC.cs
@@ -14,15 +14,29 @@ using BaseDatos;
 
 namespace SAFCore
 {
-    public partial class clsDocumentoDALC
+    public partial class clsDocumentoAdicionalDALC
     {
-        
-
-        public long consultarSiguienteDocumento(long idDocumento)
+         private Comandos m_ejecutorBaseDatos;
+        public Comandos EjecutorBaseDatos
         {
-          
+            get { return m_ejecutorBaseDatos; }
+            set { m_ejecutorBaseDatos = value; }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public clsDocumentoAdicionalDALC(Comandos ejecutorBaseDatos)
+        {
+            EjecutorBaseDatos = ejecutorBaseDatos;
+        }
+        public long consultarSiguienteDocumento(long idTipoDocumento)
+        {
+
             EjecutorBaseDatos.limpiarParametros();
-            string sentenciaSQL = " Select Numero From bdDocumento Where Id = @IdDocumento";
+            string sentenciaSQL = " Select Numero From bdDocumento Where Id = @TipoDocumento";
             //EjecutorBaseDatos.agregarParametro(filtro.Campo, filtro.Valor);
             return 0;
         }
